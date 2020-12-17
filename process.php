da22d38010969eae345f45dd87d57830747e0aad
tomas-smith <66832482+tomas-smith@users.noreply.github.com>
2020-12-17 20:06:29 +0000

    update

--- process.php
+++ process.php
@@ -16,31 +16,30 @@ if (isset($_POST['first_name'])){
     $surname = $_POST['surname'];
     $nationality = $_POST['nationality'];
     //second table 
-    $user_id = $_POST['user_id'];
+   
     $address_line_1 = $_POST['address_line_1'];
     $address_line_2 = $_POST['address_line_2'];
     $telephone = $_POST['telephone'];
     $e_mail = $_POST['e_mail'];
 
 
-    // $query1 = "INSERT INTO users(first_name, surname, nationality) VALUES ('{$first_name}', '{$surname}', '{$nationality}') ";
-    // $query2 = "INSERT INTO user_details(user_id, address_line_1, address_line_2, telephone, e_mail) VALUES ('{$user_id}', '{$address_line_1}', '{$address_line_2}', '{$telephone}', '{$e_mail}' ) ";
+     $query1 = "INSERT INTO users(first_name, surname, nationality) VALUES ('{$first_name}', '{$surname}', '{$nationality}') ";
+     $query2 = "INSERT INTO users_details(address_line_1, address_line_2, telephone, e_mail) VALUES ('{$address_line_1}', '{$address_line_2}', '{$telephone}', '{$e_mail}' ) ";
+    
+// $mquery = "INSERT INTO users(first_name, surname, nationality) VALUES ('{$first_name}', '{$surname}', '{$nationality}'); INSERT INTO users_details(address_line_1, address_line_2, telephone, e_mail) VALUES ('{$address_line_1}', '{$address_line_2}', '{$telephone}', '{$e_mail}' );";
+      $create_post_query_1 = mysqli_query($connection,$query1);
+     if(!$create_post_query_1){
+         die("QUERY FAILURE: ". mysqli_error($connection)); }
 
-    $mquery = "INSERT INTO users(first_name, surname, nationality) VALUES ('{$first_name}', '{$surname}', '{$nationality}'); INSERT INTO user_details(user_id, address_line_1, address_line_2, telephone, e_mail) VALUES ('{$user_id}', '{$address_line_1}', '{$address_line_2}', '{$telephone}', '{$e_mail}' );";
-    // $create_post_query_1 = mysqli_query($connection,$query1);
-    // if(!$create_post_query_1){
-    //     die("QUERY FAILURE: ". mysqli_error($connection));
-    // }
+     $create_post_query_2 = mysqli_query($connection,$query2);
+     if(!$create_post_query_1){
+         die("QUERY FAILURE: ". mysqli_error($connection));
+     }
 
-    // $create_post_query_2 = mysqli_query($connection,$query2);
-    // if(!$create_post_query_1){
-    //     die("QUERY FAILURE: ". mysqli_error($connection));
-    // }
-
-    $create_multi_query = mysqli_multi_query($connection, $mquery);
-    if(!$create_multi_query){
-            die("QUERY FAILURE: ". mysqli_error($connection));
-        }
+    // $create_multi_query = mysqli_multi_query($connection, $mquery);
+    // if(!$create_multi_query){
+    //         die("QUERY FAILURE: ". mysqli_error($connection));
+    //     }
 
 
 
