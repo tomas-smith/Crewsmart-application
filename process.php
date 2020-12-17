f402bcd6c6c5810ee777218e58cf6c2a140cb0b1
tomas-smith <66832482+tomas-smith@users.noreply.github.com>
2020-12-17 17:59:53 +0000

    update connection
    -created connection with the SQL server
    -created query connectins

--- process.php
+++ process.php
@@ -4,13 +4,39 @@ $connection = mysqli_connect("localhost", "root", "", "crewsmart");
 
 //confirming connection
 if($connection){
-    //echo "Connection status: confirmed ";
+    echo "Connection status: confirmed ";
 }
 ?>
 
 <?php 
 if (isset($_POST['first_name'])){
     print_r($_POST);
+    //first table
+    $first_name = $_POST['first_name'];
+    $surname = $_POST['surname'];
+    $nationality = $_POST['nationality'];
+    //second table 
+    $user_id = $_POST['user_id'];
+    $address_line_1 = $_POST['address_line_1'];
+    $address_line_2 = $_POST['address_line_2'];
+    $telephone = $_POST['telephone'];
+    $e_mail = $_POST['e_mail'];
+
+
+    $query1 = "INSERT INTO users(first_name, surname, nationality) VALUES ('{$first_name}', '{$surname}', '{$nationality}') ";
+    $query2 = "INSERT INTO user_details(user_id, address_line_1, address_line_2, telephone, e_mail) VALUES ('{$user_id}', '{$address_line_1}', '{$address_line_2}', '{$telephone}', '{$e_mail}' ) ";
+
+    $create_post_query_1 = mysqli_query($connection,$query1);
+    if(!$create_post_query_1){
+        die("QUERY FAILURE: ". mysqli_error($connection));
+    }
+
+    $create_post_query_2 = mysqli_query($connection,$query2);
+    if(!$create_post_query_1){
+        die("QUERY FAILURE: ". mysqli_error($connection));
+    }
+
+
 
 }
 
